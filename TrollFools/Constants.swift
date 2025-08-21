fc9f0f80adaaa5900e60f322688f8c2be75dd7b9
clg456852 <>
2025-08-22 02:47:24 +0800

    App 改名

--- TrollFools/Constants.swift
+++ TrollFools/Constants.swift
@@ -5,5 +5,5 @@
 //  Created by 82Flex on 3/8/25.
 //
 
-let gTrollFoolsIdentifier = "wiki.qaq.TrollFools"
+let gTrollFoolsIdentifier = "com.doo.injection"
 let gTrollFoolsErrorDomain = "\(gTrollFoolsIdentifier).error"
