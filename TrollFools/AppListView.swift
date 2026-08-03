0d98e8e3bcb94e4c83dd3551a7f8a65d6f0416c2
wy <459379621@qq.com>
2026-08-03 22:55:51 +0800

    feat:改名

--- TrollFools/AppListView.swift
+++ TrollFools/AppListView.swift
@@ -42,7 +42,10 @@ struct AppListView: View {
     }
 
     var appString: String {
-        let appNameString = Bundle.main.infoDictionary?["CFBundleName"] as? String ?? "DjInjection"
+        let appNameString =
+            (Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String) ??
+            (Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String) ??
+            "DjInjection"
         let appVersionString = String(
             format: "v%@ (%@)",
             Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.0.0",
